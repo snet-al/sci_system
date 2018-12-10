2247f5ab36571a2b28586cc8e21e7283ec6b1ee6
Bledar Shehu <shehu.bledar9@gmail.com>
2018-12-10 20:54:40 +0100

    change the constructors

--- config/Config.php
+++ config/Config.php
@@ -1,36 +1,35 @@
-<?php 
+<?php
 class SCI_Config
 {
-	public $config = [];
-	public $owner;
+    public $config = [];
+    public $_config_paths = [];
 
-	public function __construct($owner)
-	{
-		$this->owner = $owner;
-		$this->getConfig();
-	}
-	
-	public function getConfig()
-	{
-		$this->config['index_controller'] = 'home';
-		$this->config['index_method'] = 'index';
-		
-		if (isset($GLOBALS['database']) && gettype($GLOBALS['database']) == 'array') {
-			$database = $GLOBALS['database'];
-			$this->config['database_host'] = $database['host'];
-			$this->config['database_user'] = $database['user'];
-			$this->config['database_password'] = $database['password'];
-			$this->config['database_name'] = $database['database'];
-		} else {
-			$this->config['database_host'] = 'localhost';
-			$this->config['database_user'] = 'root';
-			$this->config['database_password'] = '';
-			$this->config['database_name'] = 'mysql';
-		}
-		
-		if (isset($GLOBALS['security']) && gettype($GLOBALS['security']) == 'string') {
-		    $this->config['security'] = $GLOBALS['security'];
-		}
-	}
+    public function __construct($owner)
+    {
+        $this->owner = $owner;
+        $this->getConfig();
+    }
+
+    public function getConfig()
+    {
+        global $database;
+        $this->config['index_controller'] = 'home';
+        $this->config['index_method'] = 'index';
+
+        if (isset($database) && gettype($database) == 'array') {
+            $this->config['database_host'] = $database['host'];
+            $this->config['database_user'] = $database['user'];
+            $this->config['database_password'] = $database['password'];
+            $this->config['database_name'] = $database['database'];
+        } else {
+            $this->config['database_host'] = 'localhost';
+            $this->config['database_user'] = 'root';
+            $this->config['database_password'] = '';
+            $this->config['database_name'] = 'mysql';
+        }
+
+        if (isset($GLOBALS['security']) && gettype($GLOBALS['security']) == 'string') {
+            $this->config['security'] = $GLOBALS['security'];
+        }
+    }
 }
-?>
--- core/Init.php
+++ core/Init.php
@@ -1,82 +1,98 @@
 <?php
-class SCI 
+class SCI
 {
-	public $config = null;
-	public $errors = [];
-	public $uri = null;
-	public $router = null;
-	public $db = null;
-	
-	public $controllerName;
-	public $methodName;
-	public $paramVector = array();
-	
-	public $controller = null;
-	public $view = null;
-	
-	public function __construct()
-	{
-		$this->loadConfig();
-		$this->loadUri();
-		$this->loadRouter();
-		$this->connectDb();
-		
-		$this->security();
-		
-		//if isset $auth_mode we are in the authorization process , we are in a different envirement or app menaged by security
-		// and we dont enter in mvc
-		if (! isset($GLOBALS['auth_mode']) || ! $GLOBALS['auth_mode'] || $GLOBALS['auth_mode'] != 1) {
-		    $this->startMVC();
-		}
-		
-	}
-	
-	public function loadConfig()
-	{
-		require_once(BASEPATH . 'config/Config.php');
-		$this->config = new SCI_Config($this);
-	}
+    public $config = null;
+    public $errors = [];
+    public $uri = null;
+    public $router = null;
+    public $db = null;
+    public $request = [];
 
-	public function loadUri()
-	{
-		require_once(BASEPATH . 'router/URL.php');
-		$this->uri = new SCI_URI($this);
-		$this->uri->fetchUrl($_SERVER['REQUEST_URI']);
-	}
+    public $controllerName;
+    public $methodName;
+    public $paramVector = array();
 
-	public function loadRouter()
-	{
-		require_once(BASEPATH.'router/Router.php');
-		$this->router = new SCI_Router($this);
-	}
+    public $controller = null;
+    public $view = null;
 
-	public function connectDb()
-	{
-		require_once(BASEPATH . 'database/Database.php');
-		$this->db = new Database($this->config->config['database_host'], 
-								$this->config->config['database_user'], 
-								$this->config->config['database_password'],
-								$this->config->config['database_name']);
-		if ($this->db == null) {
-			echo "DB::error";
-			exit();
-		}
-	}
+    public function __construct()
+    {
+        $this->loadConfig();
+        $this->loadUri();
+        $this->loadRouter();
+        $this->connectDb();
 
-	public function security()
-	{
-	    $this->uri->rebuildRequest();
-	    
-	    if (isset($this->config->config['security']) && $this->config->config['security'] === 'oauth2') {
-	        require_once(BASEPATH.'security/OAuth2.php');
-	        $this->security = new Oauth2($this, $this->config->config['security']);
-	        $this->security->check();
-	    }
-	}
+        $this->security();
 
-	public function startMVC()
+        //if isset $auth_mode we are in the authorization process , we are in a different envirement or app menaged by security
+        // and we dont enter in mvc
+        if (! isset($GLOBALS['auth_mode']) || ! $GLOBALS['auth_mode'] || $GLOBALS['auth_mode'] != 1) {
+            $this->loadTpl();
+            $this->startMVC();
+        }
+
+    }
+
+    public function loadConfig()
+    {
+        require_once(BASEPATH . 'config/Config.php');
+        $this->config = new SCI_Config($this);
+    }
+
+    public function loadUri()
+    {
+        require_once(BASEPATH . 'router/URL.php');
+        $this->uri = new SCI_URI($this);
+        $this->uri->fetchUrl($_SERVER['REQUEST_URI']);
+    }
+
+    public function loadRouter()
+    {
+        require_once(BASEPATH.'router/Router.php');
+        $this->router = new SCI_Router($this);
+    }
+
+    public function loadTpl()
+    {
+        if (file_exists(BASEPATH.'template/STemplate.php')) {
+            require_once (BASEPATH . 'template/STemplate.php');
+            $this->tpl = new SCI_Template($this, BASEPATH . 'template/Mustache');
+        } else {
+            $this->tpl = null;
+        }
+    }
+
+    public function connectDb()
+    {
+        require_once(BASEPATH . 'database/Database.php');
+        $this->db = new Database($this->config->config['database_host'],
+            $this->config->config['database_user'],
+            $this->config->config['database_password'],
+            $this->config->config['database_name']);
+        if ($this->db == null) {
+            echo "DB::error";
+            exit();
+        }
+    }
+
+    public function security()
+    {
+        $this->request = $this->uri->rebuildRequest();
+
+        if (isset($this->config) && isset($this->config->config['security']) && $this->config->config['security'] == 'oauth2') {
+            require_once(BASEPATH.'security/OAuth2.php');
+            $this->security = new Oauth2($this, $this->config->config['security']);
+            $this->security->check();
+        } else {
+            //default flow for ecommerce
+            //TODO: add the auth check of ecommerce with Oauth2 and the posibility to login with google and facebook
+        }
+    }
+
+    public function startMVC()
     {
         require_once('Controller.php');
+        require_once('Model.php');
 
         $this->router->fetchController();
         $this->router->fetchMethod();
@@ -103,6 +119,7 @@ class SCI
         $this->controller = new $this->controllerName();
         $this->controller->owner = $this;
         $this->controller->db = $this->db;
+        $this->controller->request = $this->request;
         $this->controller->callMethod($this->methodName);
     }
 }
